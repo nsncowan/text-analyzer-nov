870297e056027cad5225ccc2632f1f4a926585aa
Noah Cowan <nsncowan@gmail.com>
2022-11-07 20:42:24 -0800

    add numberofOccurencesInText and wordCounter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
 // Business Logic
 
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+    wordCount++;
+  }
+  });
+  return wordCount;
+}
+
+function numberOfOccurencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++
+    }
+  });
+  return wordCount;
+}
